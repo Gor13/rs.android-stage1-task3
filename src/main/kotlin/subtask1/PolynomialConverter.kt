9a637c005e94f8aa2aee734bb52e2f5cea532be4
Hardzei <pavelko1331@gmail.com>
2020-05-02 13:17:12 +0300

    init subtask1 completed

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -2,8 +2,33 @@ package subtask1
 
 class PolynomialConverter {
 
-    // TODO: Complete the following function
+    //  Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        // throw NotImplementedError("Not implemented")
+        var string: String? = ""
+        if (numbers.size == 0)
+            string = null
+        else {
+            for ((index, number) in numbers.withIndex()) {
+                var sum: String = ""
+                when (number) {
+                    0 -> sum = ""
+                    1 -> sum = " + x"
+                    -1 -> sum = " - x"
+                    else -> {
+                        val sign = if (number < 0) " - " else " + "
+
+                        val degree = if ((numbers.size - index - 1) == 0) ""
+                        else if ((numbers.size - index - 1) == 1) "x"
+                        else "x^${numbers.size - index - 1}"
+
+                        sum = sign + java.lang.Math.abs(number) + degree
+                    }
+                }
+                string = string + sum
+            }
+        }
+
+        return if (string != null) string.substring(3, string.length) else string
     }
 }
